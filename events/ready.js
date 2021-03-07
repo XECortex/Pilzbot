2b2090bdfb2f1720d981de34111bf0742449e7f7
XECortex <57626963+XECortex@users.noreply.github.com>
2021-03-07 16:34:18 +0100

    YouTube notifications

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+request = new (require("rss-parser"))();
+
 function log(data) { console.log(`\x1b[1m[INFO]\x1b[0m ${data}`); }
 
 module.exports = async function(client) {
@@ -10,4 +13,16 @@ module.exports = async function(client) {
 	setInterval(function() {
 		client.user.setPresence({ activity: { name: new rndmzr.Doge().wow() }, status: 'idle' });
 	}, 5000);
+
+	setInterval(() => {
+		request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.ytChannelId}`)
+			.then(data => {
+				lastYId = fs.readFileSync('./config/lastyid');
+
+				if (lastYId != data.items[0].id) {
+					fs.writeFile('./config/lastyid', data.items[0].id, 'utf-8', () => {});
+					client.channels.cache.find(channel => channel.id === client.config.announcementChannelId).send(`||@everyone|| Neues Video: ${data.items[0].title}\n${data.items[0].link}`);
+				}
+			});
+	}, 60000);
 }
