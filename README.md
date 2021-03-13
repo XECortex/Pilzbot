1d43bfb6fdaad872eacc6a629c454e030c53afd5
XECortex <57626963+XECortex@users.noreply.github.com>
2021-03-13 21:15:49 +0100

    Twitch notifications

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Pilzbot
 A Discord bot for Pilzschaf's [Discord server](https://discord.com/invite/RS9CuJt)
 ![Avatar](https://raw.githubusercontent.com/XECortex/Pilzbot/main/pilzbot.png)
+
+# Config
+```json
+{
+    // The directory where the source files are inside
+	"root": "/var/node/pilzbot/",
+    // Discord bot token
+	"token": "xxx",
+    // Command prefix, like !, / or -
+	"prefix": "sudo ",
+    // YouTube Channel ID for YouTube notifications
+	"ytChannelId": "xxx",
+    // Twitch stream notifications
+	"twitch": {
+        // Name of Twitch channel (not displayname)
+		"channel": "xe_cortex",
+        // Search query, most likely your channel name (Your channel should appear on https://www.twitch.tv/search when you search the query)
+		"query": "xe%20cortex",
+        // Twitch App client ID (https://dev.twitch.tv/docs/api/)
+		"clientId": "xxx",
+        // Generated secret token (https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#oauth-client-credentials-flow)
+		"token": "xxx"
+	},
+    // Discord channel ID for announcements
+	"announcementChannelId": "xxx"
+}
+```
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
-request = new (require("rss-parser"))();
+const request = new (require('rss-parser'))();
+const https = require('https');
 
 function log(data) { console.log(`\x1b[1m[INFO]\x1b[0m ${data}`); }
+function error(data) { console.log('\x1b[31m\x1b[1m[ERROR]\x1b[0m\x1b[31m', data, '\x1b[0m'); }
+
+var wasLive = false;
 
 module.exports = async function(client) {
 	log(`Client logged in as \x1b[32m${client.user.tag}\x1b[0m`);
@@ -10,19 +14,63 @@ module.exports = async function(client) {
 
 	const rndmzr = require(`${client.root}rndmzr.js`);
 
+	// Update status
 	setInterval(function() {
 		client.user.setPresence({ activity: { name: new rndmzr.Doge().wow() }, status: 'idle' });
 	}, 5000);
 
+	// Media notivications
 	setInterval(() => {
+		// YouTube
 		request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${client.config.ytChannelId}`)
 			.then(data => {
 				lastYId = fs.readFileSync('./config/lastyid');
 
 				if (lastYId != data.items[0].id) {
 					fs.writeFile('./config/lastyid', data.items[0].id, 'utf-8', () => {});
-					client.channels.cache.find(channel => channel.id === client.config.announcementChannelId).send(`||@everyone|| Neues Video: ${data.items[0].title}\n${data.items[0].link}`);
+					client.channels.cache.find(channel => channel.id === client.config.announcementChannelId).send(`||@everyone|| ${data.items[0].link}`);
 				}
 			});
-	}, 60000);
+		
+		// Twitch
+		https.get({ host: 'api.twitch.tv', path: `helix/search/channels?query=${client.config.twitch.query}`, headers: {'client-id': client.config.twitch.clientId, 'Authorization': `Bearer ${client.config.twitch.token}`}}, (res) => {
+			var body = '';
+
+			res.on('data', (chunk) => {
+				body += chunk;
+			});
+
+			res.on('end', () => {
+				var json;
+
+				try {
+					json = JSON.parse(body);
+				} catch (err) {
+					error(`Twitch API error: ${err}`);
+					return;
+				}
+
+				if (json.status == 404) {
+					error(`Twitch streamer not found: ${client.config.twitch.query}`)
+				} else {
+					// log(require('util').inspect(json));
+					for (channel of json.data) {
+						if (channel.broadcaster_login == client.config.twitch.channel) {
+							live = channel.is_live;
+
+							if (live && !wasLive) {
+								client.channels.cache.find(channel => channel.id === client.config.announcementChannelId).send(`||@everyone|| https://www.twitch.tv/${client.config.twitch.channel}`);
+							}
+						
+							wasLive = live;
+						}
+					}					
+				}
+			});
+
+			res.on('error', (err) => {
+				error(`Twitch API error: ${err}`);
+			});
+		});
+	}, 1000);
 }
